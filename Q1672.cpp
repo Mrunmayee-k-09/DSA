6775092d9fe24ad6ae9495cda17df6640fc8f1f1
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-09 17:55:45 +0530

    Add Q4 solution

---
+++ Q1672.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int i, j, n;
+        n = accounts.size();
+        int maxWealth = 0;
+        int richest = 0;
+
+        for(i=0; i<n; i++) {
+            int sum = 0;
+            for(j=0; j<accounts[i].size(); j++) {
+                    sum += accounts[i][j];
+
+                    if(sum > maxWealth) {
+                        maxWealth = sum;
+                    }
+            }
+        }
+
+        return maxWealth;
+        
+    }
+};
