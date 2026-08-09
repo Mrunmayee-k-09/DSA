3bdd59ff3d8bac147340fd959f5960500214bfa9
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-09 15:58:09 +0530

    Add Q2 solution optimal

---
+++ Q1295-opt.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+       
+        int i;
+        int n = nums.size();
+        int answer = 0;
+        
+
+        for(i=0; i<n; i++) {
+            if(to_string(nums[i]).length() % 2 == 0) {
+                answer++;
+            }
+        }
+        return answer;
+           
+    }
+};
