43c13d9bf336496f874b3ca077baaa281c6d1129
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-11 18:11:57 +0530

    Add Q13 solution

---
+++ Q2047.cpp
@@ -0,0 +1,53 @@
+// end() is not actually the last character. It's one position after it.
+
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int n = sentences.size();
+        int maximum = 0;
+
+
+        for(int i = 0; i < n; i++) {
+            int spaces = count(sentences[i].begin(), sentences[i].end(), ' ');
+            int words = spaces + 1;
+
+            maximum = max(maximum, words);
+
+        }
+
+        return maximum;
+    }
+};
+
+
+/* class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int n = sentences.size();
+        class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+
+        int maximum = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            int spaces = 0;
+
+            for(int j = 0; j < n; j++) {
+
+                if(sentences[i][j] == ' ') {
+                    spaces++;
+                }
+            }
+
+            int words = spaces + 1;
+            maximum = max(maximum, words);
+        }
+
+        return maximum;
+    }
+};
+    }
+};
+*/
