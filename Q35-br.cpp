fc4d9d20da72fecb58dbea85572346c718d12e6a
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-11 17:17:47 +0530

    Add Q11 solution

---
+++ Q35-br.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i;
+        int n = nums.size();
+
+        for(i=0; i<n; i++) {
+            if(target == nums[i]) {
+                return i;
+            }
+
+              else if(target < nums[i]) {
+                return i;
+            }
+        }
+
+        return n;
+    }
+};
