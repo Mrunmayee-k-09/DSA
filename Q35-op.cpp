7b6a863e23458aa1139f5c78bb92231e65ef224e
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-11 17:29:26 +0530

    Add Q12 solution

---
+++ Q35-op.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i;
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        
+        while(left<=right) {
+
+            int mid = (left + right)/2;
+
+            if(target == nums[mid]) {
+                return mid;
+            }
+
+            else if(target<nums[mid]) {
+                right = mid - 1;
+            }
+
+            else {
+                left = mid+1;
+            }
+
+        }
+        return left;
+    }
+};
