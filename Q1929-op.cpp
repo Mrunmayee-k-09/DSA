9cff436eae9118ccad888cc92977eeb2d70a6b8a
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-11 17:06:11 +0530

    Add Q11 solution

---
+++ Q1929-op.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int i,j;
+        int n = nums.size();
+        vector<int>ans(2*n);
+        for(i=0; i<n; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+            
+        }
+       
+        return ans;
+    }
+};
