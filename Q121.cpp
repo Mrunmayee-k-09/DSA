87b8a694bdce0d70cbb94684ab4c100f40529388
Mrunmayee <312085014+Mrunmayee-k-09@users.noreply.github.com>
2026-08-21 12:40:58 +0530

    Add solution

---
+++ Q121.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = INT_MIN;
+        int buy = INT_MAX;
+
+        int n = prices.size();
+
+        for(int i = 0; i < n; i++) {
+
+            buy = min(buy, prices[i]);
+
+            int sell = prices[i];
+
+            maxProfit = max(maxProfit, sell - buy);
+        }
+
+        return maxProfit;
+    }
+};
